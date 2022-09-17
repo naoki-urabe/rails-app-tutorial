e3b1e2fba77fa493d157d125dcf4abc41b027f44
nao_e <nao_e@nao-desktop-win.localdomain>
2022-09-17 22:19:16 +0900

    instance起動設定追加

--- infra/instance.tf
+++ infra/instance.tf
@@ -14,10 +14,52 @@ resource "aws_subnet" "for-tutorial-public-subnet" {
   }
 }
 
-resource "aws_network_interface" "for-tutorial-network-interface" {
-  subnet_id = aws_subnet.for-tutorial-public-subnet.id
-  tags = {
-    Name = "for-tutorial-network-interface"
+resource "aws_internet_gateway" "for-rails-tutorial" {
+  vpc_id = aws_vpc.for-tutorial.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.for-tutorial.id
+}
+
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.for-rails-tutorial.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.for-tutorial-public-subnet.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "for-rails-tutorial" {
+  name        = "for-rails-tutorial"
+  description = "for rails tutorial"
+  vpc_id      = aws_vpc.for-tutorial.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
@@ -26,10 +68,16 @@ provider "aws" {
 }
 
 resource "aws_instance" "instance" {
-  ami           = "ami-0f36dcfcc94112ea1"
-  instance_type = "t2.micro"
-  network_interface {
-    network_interface_id = aws_network_interface.for-tutorial-network-interface.id
-    device_index         = 0
-  }
+  subnet_id                   = aws_subnet.for-tutorial-public-subnet.id
+  ami                         = "ami-0f36dcfcc94112ea1"
+  instance_type               = "t2.micro"
+  key_name                    = "udemysample"
+  security_groups             = [aws_security_group.for-rails-tutorial.id]
+  associate_public_ip_address = true
+  user_data                   = <<EOF
+  #!/bin/bash
+  sudo yum update 
+  sudo yum install -y docker
+  sudo service docker start
+  EOF
 }
